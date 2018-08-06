914452cf573346818abdee2e184cf7efc1ab265f
David Beauchamp <david@xtuple.com>
2018-08-05 23:00:59 -0400

    0.1.2: Better guarding against invalid input, adding support for rotation

--- pgzint.c
+++ pgzint.c
@@ -114,33 +114,97 @@ bc_generate(PG_FUNCTION_ARGS)
     struct zint_symbol *barcode;
     unsigned char *input;
     int error = 0;
+    int rotation_angle = 0;
     barcode = ZBarcode_Create();
     barcode->input_mode = UNICODE_MODE;
 
-    if (PG_ARGISNULL(0))
+    if (PG_ARGISNULL(0)) {
+        ZBarcode_Delete(barcode);
         ereport(ERROR, (errmsg("Error creating barcode"),
-                        errdetail("No input provided"),
-                        errhint("Input is required to generate a barcode")));
+                        errdetail("No input text provided"),
+                        errhint("Input text is required to generate a barcode")));
+    }
     else
        input = uchar_p_from_text_p(PG_GETARG_TEXT_P(0));
 
-    if (!PG_ARGISNULL(1))
-        barcode->symbology = PG_GETARG_INT32(1);
+    if (!PG_ARGISNULL(1)) {
+        int symbology = PG_GETARG_INT32(1);
+        if (symbology >= 1) {
+            barcode->symbology = symbology;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid symbology provided: %d", symbology),
+                            errhint("Symbology must be greater than or equal to 1. "
+                                    "This is not checked against the list of symbologies so the caller must ensure validity.")));
+        }
+    }
 
-    if (!PG_ARGISNULL(2))
-        barcode->height = PG_GETARG_INT32(2);
+    if (!PG_ARGISNULL(2)) {
+        int height = PG_GETARG_INT32(2);
+        if (height >= 0) {
+            barcode->height = height;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid height provided: %d", height),
+                            errhint("Scale must be greater than or equal to 0")));
+        }
+    }
 
-    if (!PG_ARGISNULL(3))
-        barcode->scale = PG_GETARG_INT32(3);
+    if (!PG_ARGISNULL(3)) {
+        int scale = PG_GETARG_INT32(3);
+        if (scale >= 0.01) {
+            barcode->scale = scale;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid scale provided: %d", scale),
+                            errhint("Scale must be greater than or equal to 0.01")));
+        }
+    }
 
-    if (!PG_ARGISNULL(4))
-        barcode->whitespace_width = PG_GETARG_INT32(4);
+    if (!PG_ARGISNULL(4)) {
+        int whitespace_width = PG_GETARG_INT32(4);
+        if (whitespace_width >= 0 && whitespace_width <= 1000) {
+            barcode->whitespace_width = whitespace_width;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid whitespace width provided: %d", whitespace_width),
+                            errhint("Whitespace width must be between 0 to 1000")));
+        }
+    }
 
-    if (!PG_ARGISNULL(5))
-        barcode->border_width = PG_GETARG_INT32(5);
+    if (!PG_ARGISNULL(5)) {
+        int border_width = PG_GETARG_INT32(5);
+        if (border_width >= 0 && border_width <= 1000) {
+            barcode->border_width = border_width;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid border width provided: %d", border_width),
+                            errhint("Border width must be between 0 to 1000")));
+        }
+    }
 
-    if (!PG_ARGISNULL(6))
-        barcode->output_options = PG_GETARG_INT32(6);
+    if (!PG_ARGISNULL(6)) {
+        int output_options = PG_GETARG_INT32(6);
+        if (output_options >= 0) {
+            barcode->output_options = output_options;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid output option provided: %d", output_options),
+                            errhint("Output options must be greater than or equal to 0")));
+        }
+    }
 
     if (!PG_ARGISNULL(7))
         strcpy(barcode->fgcolour, VARDATA(PG_GETARG_TEXT_P(7)));
@@ -157,23 +221,61 @@ bc_generate(PG_FUNCTION_ARGS)
     else
         barcode->show_hrt = 0;
 
-    if (!PG_ARGISNULL(10))
-        barcode->option_1 = PG_GETARG_INT32(10);
+    if (!PG_ARGISNULL(10)) {
+        int option_1 = PG_GETARG_INT32(10);
+        if (option_1 >= 0) {
+            barcode->option_1 = option_1;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid option provided: %d", option_1),
+                            errhint("Option 1 must be greater than or equal to 0")));
+        }
+    }
 
-    if (!PG_ARGISNULL(11))
-        barcode->option_2 = PG_GETARG_INT32(11);
+    if (!PG_ARGISNULL(11)) {
+        int option_2 = PG_GETARG_INT32(11);
+        if (option_2 >= 0) {
+            barcode->option_2 = option_2;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid option provided: %d", option_2),
+                            errhint("Option 2 must be greater than or equal to 0")));
+        }
+    }
 
-    if (!PG_ARGISNULL(12))
-        barcode->option_3 = PG_GETARG_INT32(12);
+    if (!PG_ARGISNULL(12)) {
+        int option_3 = PG_GETARG_INT32(12);
+        if (option_3 >= 0) {
+            barcode->option_3 = option_3;
+        }
+        else {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid option provided: %d", option_3),
+                            errhint("Option 3 must be greater than or equal to 0")));
+        }
+    }
 
-    error = ZBarcode_Encode_and_Buffer(barcode, input, 0, 0);
+    if (!PG_ARGISNULL(13)) {
+        rotation_angle = PG_GETARG_INT32(13);
+        if (rotation_angle != 0 && rotation_angle != 90 && rotation_angle != 180 && rotation_angle != 270) {
+            ZBarcode_Delete(barcode);
+            ereport(ERROR, (errmsg("Error creating barcode"),
+                            errdetail("Invalid rotation angle provided: %d", rotation_angle),
+                            errhint("Rotation angle must be either 0, 90, 180, or 270, defaults to 0")));
+        }
+    }
+
+    error = ZBarcode_Encode_and_Buffer(barcode, input, 0, rotation_angle);
     // zint changed ZWARN_INVALID_OPTION to ZINT_WARN_INVALID_OPTION
     // both values are 2, so to keep compatible I just use the magic number
-    if (error >= 2)
-    {
-        ereport(ERROR, (errmsg("%s", barcode->errtxt)));
+    if (error >= 2) {
         ZBarcode_Delete(barcode);
-        PG_RETURN_NULL();
+        ereport(ERROR, (errmsg("Zint Error: %s", barcode->errtxt)));
     }
     
     result = png_from_barcode(barcode);
